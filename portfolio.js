5d7488e91d8403e664e3d0e6f647019e17ec984a
Erika <erikasamp7@gmail.com>
2023-02-17 17:50:24 -0500

    deploy pages

--- portfolio.js
+++ portfolio.js
@@ -1,20 +1,25 @@
 const images = document.querySelectorAll('.gallery-image');
 let index = 0;
 
-// Set the first image as active
-images[index].classList.add('active');
+document.addEventListener('DOMContentLoaded', function () {
+  // Set the first image as active
+  images[index].classList.add('active');
 
-// Add event listeners to the images
-images.forEach((image, i) => {
-  image.addEventListener('click', () => {
-    // Remove the active class from the current image
-    images[index].classList.remove('active');
+  // Add event listeners to the images
+  images.forEach((image, i) => {
+    image.addEventListener('click', () => {
+      // Remove the active class from the current image
+      images[index].classList.remove('active');
 
-    // Set the clicked image as active
-    index = i;
-    images[index].classList.add('active');
+      // Set the clicked image as active
+      index = i;
+      images[index].classList.add('active');
+    });
   });
 });
+
+
+
 
   
   
