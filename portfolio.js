168a67e6f5512a4ec098e3d8da5bb8cb746367fd
Erika <erikasamp7@gmail.com>
2023-02-17 18:16:58 -0500

    deploy pages

--- portfolio.js
+++ portfolio.js
@@ -1,23 +1,40 @@
+
 const images = document.querySelectorAll('.gallery-image');
-let index = 0;
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
+let currentImage = 0;
 
 document.addEventListener('DOMContentLoaded', function () {
-  // Set the first image as active
-  images[index].classList.add('active');
+  // Set the first image to be active
+  images[currentImage].classList.add('active');
 
-  // Add event listeners to the images
-  images.forEach((image, i) => {
-    image.addEventListener('click', () => {
-      console.log("registered click");
-      // Remove the active class from the current image
-      images[index].classList.remove('active');
+  // Add event listeners to the buttons
+  prevButton.addEventListener('click', showPrevImage);
+  nextButton.addEventListener('click', showNextImage);
 
-      // Set the clicked image as active
-      index = i;
-      images[index].classList.add('active');
-    });
-  });
+  function showPrevImage() {
+    images[currentImage].classList.remove('active');
+    currentImage--;
+    if (currentImage < 0) {
+      currentImage = images.length - 1;
+    }
+    images[currentImage].classList.add('active');
+  }
+
+  function showNextImage() {
+    images[currentImage].classList.remove('active');
+    currentImage++;
+    if (currentImage >= images.length) {
+      currentImage = 0;
+    }
+    images[currentImage].classList.add('active');
+  }
 });
+
+
+
+
+
 
 
 
